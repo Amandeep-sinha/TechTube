558e327cbd5c2fce1ddb0b2d74e86a40057b2345
Amandeep <mramandeepsinha@gmail.com>
2022-06-16 14:27:25 +0530

    Added : Pages and Routing

--- src/pages/History/HistoryCard.js
+++ src/pages/History/HistoryCard.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import "./VideoCard.css";
 import { Link } from "react-router-dom";
 
-export default function VideoCard() {
+export function HistoryCard() {
   const [showList, setShowList] = useState(false);
 
   return (
     <div className="card">
-      <Link to="singleVideo">
+      <Link to="/singleVideo">
         <img
           className="card-img"
           src="https://i.ytimg.com/vi/KUJsaM-hAjs/sddefault.jpg"
@@ -16,7 +15,7 @@ export default function VideoCard() {
       </Link>
       <div className="card-info">
         <div className="card-title">
-          <h3 className="card-title-header">First React App</h3>
+          <h3 className="card-title-header">Do Epic Shit Review</h3>
           <div className="ellipse" onClick={() => setShowList(!showList)}>
             <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
             <div
@@ -32,12 +31,16 @@ export default function VideoCard() {
                 <i className="fa fa-play-circle" aria-hidden="true"></i>
                 Add to Playlist
               </div>
+              <div>
+                <i className="fa fa-trash" aria-hidden="true"></i>
+                Remove from History
+              </div>
             </div>
           </div>
         </div>
 
         <div className="card-description">
-          <h3>Ankur Warikoo : 475749view</h3>
+          <h3>Tanay Pratap : 5965467 view</h3>
         </div>
       </div>
     </div>
--- src/pages/Playlist/component/PlaylistFolder.js
+++ src/pages/Playlist/component/PlaylistFolder.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
-import "./VideoCard.css";
 import { Link } from "react-router-dom";
+import "../Playlist.css";
 
-export default function VideoCard() {
+export default function PlaylistFolder() {
   const [showList, setShowList] = useState(false);
 
   return (
     <div className="card">
-      <Link to="singleVideo">
+      <Link to="playlistList">
         <img
           className="card-img"
           src="https://i.ytimg.com/vi/KUJsaM-hAjs/sddefault.jpg"
           alt=""
         />
+        <div className="card-highlight flex-center">
+          <i className="fa fa-play-circle" aria-hidden="true"></i>
+          <p>1+</p>
+        </div>
       </Link>
       <div className="card-info">
         <div className="card-title">
-          <h3 className="card-title-header">First React App</h3>
+          <input
+            className="card-title-header"
+            type="text"
+            value="My Colection 1"
+            disabled
+          />
+
           <div className="ellipse" onClick={() => setShowList(!showList)}>
             <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
             <div
@@ -25,19 +35,15 @@ export default function VideoCard() {
               }`}
             >
               <div>
-                <i className="fa fa-clock-o" aria-hidden="true"></i>
-                Add to Watch Later
+                <i className="fa fa-edit" aria-hidden="true"></i>
+                Edit Playlist Name
               </div>
               <div>
-                <i className="fa fa-play-circle" aria-hidden="true"></i>
-                Add to Playlist
+                <i className="fa fa-trash" aria-hidden="true"></i>
+                Delete Playlist
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="card-description">
-          <h3>Ankur Warikoo : 475749view</h3>
         </div>
       </div>
     </div>
